13ca30fc8d74275387a6c2a50c28ada3ba076650
michalspano <speko.spano@gmail.com>
2021-11-19 22:34:55 +0100

    Add col.py Class

---
+++ test/col.py
@@ -0,0 +1,9 @@
+# Store ANSI escape sequences for colors in a class
+class Colors:
+    RED: str = '\033[91m'
+    GREEN: str = '\033[92m'
+    YELLOW: str = '\033[93m'
+    BLUE: str = '\033[94m'
+    MAGENTA: str = '\033[95m'
+    CYAN: str = '\033[96m'
+    RESET: str = '\033[0m'
